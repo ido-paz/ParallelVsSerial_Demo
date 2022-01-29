fd5a492bfd83486d149820144ec382d2d439afdf
I do <ido_p_a@hotmail.com>
2022-01-29 22:28:47 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,69 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Net;
 using System.Threading.Tasks;
+using WebPageCrawler;
 
-namespace ParallelVsSerialDemo
+namespace WebPageCrawler_Task
 {
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> addresses = new List<string>()//create a websites address list
-            {"http://msn.co.il","https://yahoo.com","https://cnn.com","https://bbc.com","https://ynet.co.il",
-             "http://youtube.com","https://facebook.com","https://jango.com","https://soundcloud.com","https://rotter.net"};
+            List<string> urlList = new List<string>()//create a WebPage address list
+                    {
+                     "https://msn.co.il","https://yahoo.com","https://walla.co.il",
+                     "https://ynet.co.il","https://youtube.com","https://facebook.com",
+                     "https://jango.com","https://soundcloud.com","https://rotter.net"
+                    };
             //
             Console.WriteLine("----------");
-            PrintWebsitesData_Serial(addresses);
+            PrintWebPageData_Parralel(urlList); 
             Console.WriteLine("----------");
-            PrintWebsitesData_Parralel(addresses);
+            PrintWebPageData_Serial(urlList);
         }
         
-        ///print the websites data in the list in serial manner
-        static void PrintWebsitesData_Serial(List<string> addresses)
+        ///print the WebPage data in the list in serial manner
+        static void PrintWebPageData_Serial(List<string> urlList)
         {
-            Console.WriteLine($"{DateTime.Now}:PrintWebsitesData_Serial started");
+            Console.WriteLine($"{DateTime.Now}:PrintWebPageData_Serial started");
             //
             Stopwatch stopwatch =new Stopwatch();
             stopwatch.Start();
             //
-            foreach (string address in addresses)//print webSiteData
-                PrintWebsiteData(address);
+            foreach (string url in urlList)//print webPageData
+                PrintWebPageData(url);
             //
             stopwatch.Stop();          
             //
-            Console.WriteLine($"{DateTime.Now}:PrintWebsitesData_Serial ended and took {stopwatch.Elapsed}");
+            Console.WriteLine($"{DateTime.Now}:PrintWebPageData_Serial ended and took {stopwatch.Elapsed}");
         }
         
-        ///print the websites data in the list in Parralel manner
-        static void PrintWebsitesData_Parralel(List<string> addresses)
+        ///print the WebPage data in the list in Parralel manner
+        static void PrintWebPageData_Parralel(List<string> urlList)
         {
-            Console.WriteLine($"{DateTime.Now}:PrintWebsitesData_Parralel started");
+            Console.WriteLine($"{DateTime.Now}:PrintWebPageData_Parralel started");
             //
             Stopwatch stopwatch =new Stopwatch();
             List<Task> pwsdTaskList = new List<Task>(); 
             stopwatch.Start();
             //
-            foreach (string address in addresses)//run tasks that prints webSiteData 
-                pwsdTaskList.Add(Task.Run(()=>PrintWebsiteData(address)));
-            //
+            //Parallel.ForEach(urlList, (url) => PrintWebPageData(url));
+            //Parallel.For(0, urlList.Count, (i) => PrintWebPageData(urlList[i]));
+            foreach (string url in urlList)//run tasks that prints webpagedata 
+                pwsdTaskList.Add(Task.Run(() => PrintWebPageData(url)));
             Task.WaitAll(pwsdTaskList.ToArray());
             //
             stopwatch.Stop();             
             //
-            Console.WriteLine($"{DateTime.Now}:PrintWebsitesData_Parralel ended and took {stopwatch.Elapsed}");
+            Console.WriteLine($"{DateTime.Now}:PrintWebPageData_Parralel ended and took {stopwatch.Elapsed}");
+        }
+        ///print web site data function
+        static void PrintWebPageData(string url)
+        {
+            try
+            {
+                WebPageData webPageData = WebPageData.Get(url);
+                Console.WriteLine($"Address:{webPageData.URL},Size:{webPageData.Length},DownloadDuration:{ webPageData.DownloadElapsedMiliSeconds}");
+            }
+            catch (Exception e )
+            {
+                Console.WriteLine(e.Message);
+            }            
         }
 
-        ///print web site data function
-        static void PrintWebsiteData(string address)
-        {
-            WebClient webClient = new WebClient();
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            byte[] data = webClient.DownloadData(address);
-            stopwatch.Stop();
-            Console.WriteLine($"Address:{address},Size:{data.Length},DownloadDuration:{ stopwatch.Elapsed}");
-        }
     }
 }
--- WebSiteData.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-public class WebSiteData
-{
-    public string Address;
-    public TimeSpan DownloadDuration;
-    public int DataSize;
-} 
